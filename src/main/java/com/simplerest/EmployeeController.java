aefede57a251a55f5ccaf32b1d3263842700ec9e
unknown <vskreddy652@yahoo.com>
2018-10-17 03:53:58 +0530

    Initial commit

---
+++ src/main/java/com/simplerest/EmployeeController.java
@@ -0,0 +1,20 @@
+package com.simplerest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	
+	@RequestMapping("/abc")
+    public List<Employee> getEmployees() 
+    {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
+		return employeesList;
+    }
+
+}
